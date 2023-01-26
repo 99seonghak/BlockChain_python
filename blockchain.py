4a9bf88bdba14fd4c86c3ee8d05188875e70d509
Seonghak Kim <jip9821@naver.com>
2023-01-26 18:22:41 +0900

    파이썬으로 블록체인 구현(하)-완

--- blockchain.py
+++ blockchain.py
@@ -45,12 +45,12 @@ class Blockchain(object):
     def __init__(self):
         self.chain = [] # 블록체인, 블록이 하나씩 들어감 배열형태로 저장
         self.current_transaction = [] # 임시 트랜잭션이 들어감, Alice가 bob에게 얼마를 줬다 거래내역
-    
+        self.nodes = set() # node 목록을 보관 *같은 노드들일 경우, 한 번만 저장하게 만드는 코드 set 자료 형태
         # genesis block 첫번째 블록, 가장 첫번째 블록에 hash를 지정해두고
         self.new_block(previous_hash= 1, proof=100)  # Genesis block 생성
     
     # new_block 함수 생성 [사전 형태로 작성, key, value의 쌍]
-    def new_block(self, proof, previous_hash=None):
+    def new_block(self, proof, previous_hash):
         block = {
             'index' : len(self.chain)+1,
             'timestamp' : time(), # timestamp from 1970
@@ -80,6 +80,8 @@ class Blockchain(object):
         # 해시라는 함수를 어떨때 직접 짜냐? - json형태로 뽑혀진 블록을 전체 해쉬하기 위해서!
         return hashlib.sha256(block_string).hexdigest() # 무결성 체크하며 저장
 
+    def last_block(self):
+        return self.chain[-1]
     # 작업증명
     # 전 proof p와 현재 구해야할 proof p' hash[p+p']의 앞 4자리 == "0000" 이러한 p'를 구함
     def pow(self, last_proof):
@@ -92,3 +94,55 @@ class Blockchain(object):
         guess = str(last_proof + proof).encode() # 전 proof와 다음 proof 문자열 연결
         guess_hash = hashlib.sha256(guess).hexdigest() # 이 hash값 저장
         return guess_hash[:4] == "0000" # nonce 앞 4자리가 "0000"이면 True
+    
+    # 1. Register : 새로운 노드 가입
+    # 2. Consensus : 어떤 체인이 맞는 것인지에 대해 합의
+    # 길이가 제일 긴 체인을 채택
+
+    # reigister_node    
+    def reigister_node(self, address): # address <- url 주솔르 넣게 됨
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc) # set에 저장
+    # valid_chain
+    # resolve_conflicts
+    # 한 노드들이 다른 노드와 다른 체인을 가지고 있을 때, "가장 긴 체인이 유효하다"를 위의 2개 함수를 이용하여 나타냄
+    
+    # valid_chain *유효한 트랙잭션임을 검증
+    # 이때는 새로운 다른 체인이 있을 경우에, 그 체인을 처음부터 쭉 비교. *틀린 부분이 있으면 False 반환
+    def valid_chain(self,chain):
+        last_block = chain[0] # 첫 번째 제네시스 블록이 저장됨
+        current_index = 1
+        
+        while current_index < len(chain): # 전체 체인 길이만큼 반복해 비교
+            block = chain[current_index]
+            print('%s', last_block)
+            print('%s', block)
+            print("\n----------\n")
+            # check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            last_block = block
+            current_index += 1
+        return True
+    
+    # Consensus 과정 = resolve_conflicts
+    def resolve_conflicts(self):
+        neighbours = self.nodes # 각 노드들을 저장하는 neighbours 이웃들
+        new_chain = None
+        
+        max_length = len(self.chain) # Our chain length
+        for node in neighbours:
+            tmp_url = 'http://' + str(node) + '/chain' # URL을 받아서 request를 통해 체인 정보 저장 *각 노드의 체인 함수를
+            response = requests.get(tmp_url) # 실제 request하는 부분
+            if response.status_code == 200: # 정상적으로 request를 보내고 응답을 받을 때
+                length = response.json()['length'] # 체인의 길이와 정보 저장
+                chain = response.json()['chain']
+                
+                if length > max_length and self.valid_chain(chain): # 긴 체인을 비교
+                    max_length = length
+                    
+            if new_chain:
+                self.chain = new_chain
+                return True
+            
+            return False
--- server.py
+++ server.py
@@ -5,12 +5,11 @@
 # /mine : server에게 새 블록 채굴 요청
 
 
-import json
 from flask import Flask, jsonify
-from time import time
-from textwrap import dedent
 from uuid import uuid4
+import requests
 
+# Our blockchain.py API
 from blockchain import Blockchain
 
     
@@ -23,6 +22,7 @@ blockchain = Blockchain()
 
 # /mine
 # Coinbase transaction
+@app.route('/mine',methods=['GET'])
 def mine():
     last_block = blockchain.last_block
     last_proof = last_block['proof']
@@ -50,8 +50,9 @@ def mine():
 # /transaction/new
 # POST 형식으로 데이터를 보냄
 # 이때, *POST란? url에 데이터를 붙이는 GET방식과 다르게 숨겨서 보내는 방식
+@app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    values = request.get_json() # json형태를 받아서 저장
+    values = requests.get_json() # json형태를 받아서 저장
     
     required = ['sender', 'recipient', 'amount'] # 해당 데이터가 존재해야함 얼마를 보내는지 
     if not all(k in values for k in required): # 만약 해당 데이터가 없으면 에러 뜸 400
@@ -64,6 +65,7 @@ def new_transaction():
     return jsonify(response), 201 # json 형태로 반환
 
 # /chain
+@app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
         'chain' : blockchain.chain, # 블록체인을 출력
@@ -71,12 +73,41 @@ def full_chain():
     }
     return jsonify(response), 200 # json 형태로 리턴
     # 200은 웹사이트 에러가 없을 때 뜸
+
+# /nodes/register
+@app.route('/nodes/register',methods=['POST'])
+def register_nodes():
+    values = requests.get_json() # json 형태로 보내기
     
-
-
-
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+    
+    for node in nodes: # 노드 등록 *
+        blockchain.register_node(node)
         
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes':list(blockchain.nodes),
+    }
+    return jsonify(response), 201
 
+# /nodes/resolve
+@app.route('/nodes/reslove', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts() # True False return
+    
+    if replaced: # 체인 변경 알림 메시지
+        response = {
+            'message' : 'Our chain was replaced',
+            'new_chain' : blockchain.chain
+        }
+    else:
+        response = {
+            'messagr' : 'Our chain is authoritative',
+            'chain' : blockchain.chain
+        }
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
